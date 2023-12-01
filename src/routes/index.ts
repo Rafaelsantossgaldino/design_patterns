d50197f9fca6aca47b66251ab6c3d1d5be15738b
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2023-12-01 18:44:35 -0300

    chore: add routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
 
 
-const routes = Router();
+const routes = Router(); 
 
 routes.get('/api/products', ProductController.findall)
+routes.get('/api/products/:id', ProductController.findOne)
 routes.post('/api/products', ProductController.create)
+routes.put('/api/products/:id', ProductController.update)
+routes.delete('/api/products/:id', ProductController.delete)
 
 
 export default routes
